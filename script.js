ed586e404c5d71b01821f25477f09cc69e4b1036
Viewls2 <mahasamut.kh@gmail.com>
2023-03-13 13:52:25 +0700

    feat(1): random number and print into day

---
+++ script.js
@@ -0,0 +1,17 @@
+let dayNumber = Math.floor(Math.random()*7) //random day from 0-6(quantity of 7)
+console.log(dayNumber); //should be number
+if (dayNumber === 6) {
+    console.log('Saturday');
+}   else if (dayNumber === 5) {
+    console.log('Friday');
+}   else if (dayNumber === 4) {
+    console.log('Thursday');
+}   else if (dayNumber === 3) {
+    console.log('Wednesday')
+}   else if (dayNumber === 2) {
+    console.log('Tuesday');
+}   else if (dayNumber === 1) {
+    console.log('Monday');
+}   else if (dayNumber === 0) {
+    console.log('Sunday');
+}
